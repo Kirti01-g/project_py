dae9ce635391673892f039518897252e89d4cf11
Kirti <kirtigarg871@gmail.com>
2026-02-05 13:59:14 +0530

    Add leap year calculator
    Implement leap year calculation logic using python

---
+++ leap_year_calculater.py
@@ -0,0 +1,11 @@
+# this is leap calculater
+
+year = int(input("Enter a year: "))
+
+if (year % 4 == 0):
+    if (year % 100 == 0):
+        if (year % 400 == 0):
+            print(f"{year} is a Leap Year!")
+        else:
+            print(f"{year} is not a Leap Year.")
+   
